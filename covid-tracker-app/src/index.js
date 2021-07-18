3f80a818f9d36f7c1fea6816cce3dfbd1d5b89a8
Kyle Saburao <ksaburao@sfu.ca>
2021-07-18 15:42:30 -0700

    Update index.html head

--- covid-tracker-app/src/index.js
+++ covid-tracker-app/src/index.js
@@ -5,10 +5,6 @@ import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-    />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
