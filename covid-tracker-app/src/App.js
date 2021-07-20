6c6e644b0270d3778e471bbf1d8c7b8d6f2f6ea3
Kyle Saburao <ksaburao@sfu.ca>
2021-07-20 15:09:38 -0700

    Extract API outside of react components

--- covid-tracker-app/src/App.js
+++ covid-tracker-app/src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 
 import Card from "@material-ui/core/Card";
+import * as api from "./api/api";
 
 const Summary = ({ data }) => (
   <Card>
@@ -26,35 +26,12 @@ function App() {
   const [tempData, setTempData] = useState({});
   const [ready, setReady] = useState(false);
 
-  const API_URL = "http://localhost:81/proxy";
-  const API_LOCATIONS = {
-    summary: "/summary",
-  };
-
   useEffect(() => {
-    axios.get(`${API_URL}${API_LOCATIONS.summary}`).then((res) => {
-      const createDataTitle = (title) => {
-        return title
-          .split("_")
-          .map((word) => {
-            return word.length > 0
-              ? `${word.charAt(0).toLocaleUpperCase()}${word.substring(1)}`
-              : word;
-          })
-          .join(" ");
-      };
-
-      const data = res.data.data[0];
-      const displayedData = {};
-
-      for (let key in data) {
-        displayedData[createDataTitle(key)] = data[key];
-      }
-
-      setTempData(displayedData);
+    api.getSummary((data) => {
+      setTempData(data);
       setReady(true);
     });
-  }, [API_LOCATIONS.summary]);
+  }, []);
 
   return (
     <div>
